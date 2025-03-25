9adf13535256e0206bb7e55a1d11f302ffd8fc97
hugodiaz1974 <hugotaylor@gmail.com>
2025-03-25 17:06:29 -0500

    error en urls.py

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpRespond("<h1>Bienvenido</h1>")
+    return HttpResponse("<h1>Bienvenido</h1>")
 
 # Create your views here.
